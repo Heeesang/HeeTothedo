e9c5f41724036c1c2edbd566a814f97a78d92811
Heesang <80827042+Heeesang@users.noreply.github.com>
2022-06-28 21:04:42 +0900

    :zap: calendar
    calendar layout

---
+++ HeeTothedo/HeeTothedo/ViewModels/TodoTableViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TodoTableViewModel.swift
+//  HeeTothedo
+//
+//  Created by 곽희상 on 2022/06/28.
+//
+
+import Foundation
--- HeeTothedo/HeeTothedo/Views/TodoViewController.swift
+++ HeeTothedo/HeeTothedo/Views/TodoViewController.swift
@@ -1,28 +1,20 @@
-//
-//  MainVc.swift
-//  HeeTothedo
-//
-//  Created by 곽희상 on 2022/06/27.
-//
-
-import Foundation
 import UIKit
 import FSCalendar
 import SnapKit
 import Then
 
-class todoViewController: UIViewController{
+class TodoViewController: UIViewController{
     
     let calendar = FSCalendar().then {
         $0.headerHeight = 50
         $0.appearance.headerMinimumDissolvedAlpha = 0.0
         $0.appearance.headerDateFormat = "YYYY년 M월"
         $0.appearance.headerTitleColor = .black
-        $0.appearance.headerTitleFont = UIFont.systemFont(ofSize: 24)
+        $0.appearance.headerTitleFont = UIFont.systemFont(ofSize: 21)
         $0.scrollDirection = .horizontal
-        $0.setScope(.week, animated: true)
-        
     }
+    
+    let dateFormatter = DateFormatter()
     
     
     override func viewDidLoad() {
@@ -33,43 +25,31 @@ class todoViewController: UIViewController{
         calendar.delegate = self
         calendar.dataSource = self
         
+        dateFormatter.dateFormat = "yyyy-MM-dd"
         
         view.addSubview(calendar)
         setCalendarLayout()
         
-        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(swipeEvent(_:)))
-        swipeUp.direction = .up
-        self.view.addGestureRecognizer(swipeUp)
-
-        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(swipeEvent(_:)))
-        swipeDown.direction = .down
-        self.view.addGestureRecognizer(swipeDown)
+        
     }
     
     func setCalendarLayout(){
         calendar.snp.makeConstraints{
-            $0.width.equalToSuperview()
-            $0.height.equalToSuperview()
-            $0.top.equalTo(view.safeAreaLayoutGuide)
-        }
-    }
-    
-    @objc func swipeEvent(_ swipe: UISwipeGestureRecognizer) {
-
-        if swipe.direction == .up {
-            calendar.setScope(.week, animated: true)
-        } else if swipe.direction == .down {
-            calendar.setScope(.month, animated: true)
+            $0.height.equalToSuperview().dividedBy(3)
+            $0.width.equalToSuperview().dividedBy(1.2)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(10)
+            $0.centerX.equalToSuperview()
         }
     }
 }
 
-extension todoViewController: FSCalendarDelegate, FSCalendarDataSource {
-    
-    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
-       
-        UIView.animate(withDuration: 0.5) {
-            self.view.layoutIfNeeded()
-        }
+extension TodoViewController: FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
+    // 날짜 선택 시 콜백 메소드
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(dateFormatter.string(from: date) + " 선택됨")
+    }
+    // 날짜 선택 해제 시 콜백 메소드
+    public func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(dateFormatter.string(from: date) + " 해제됨")
     }
 }
