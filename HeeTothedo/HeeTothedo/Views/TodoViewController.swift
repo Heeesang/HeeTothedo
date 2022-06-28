b4a49e366b54b9959205a49851499003084eb986
Heesang <80827042+Heeesang@users.noreply.github.com>
2022-06-28 16:15:10 +0900

    calendar

--- HeeTothedo/HeeTothedo/Views/TodoViewController.swift
+++ HeeTothedo/HeeTothedo/Views/TodoViewController.swift
@@ -11,7 +11,7 @@ import FSCalendar
 import SnapKit
 import Then
 
-class todoViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource{
+class todoViewController: UIViewController{
     
     let calendar = FSCalendar().then {
         $0.headerHeight = 50
@@ -19,7 +19,11 @@ class todoViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSo
         $0.appearance.headerDateFormat = "YYYY년 M월"
         $0.appearance.headerTitleColor = .black
         $0.appearance.headerTitleFont = UIFont.systemFont(ofSize: 24)
+        $0.scrollDirection = .horizontal
+        $0.setScope(.week, animated: true)
+        
     }
+    
     
     override func viewDidLoad() {
         super .viewDidLoad()
@@ -29,8 +33,17 @@ class todoViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSo
         calendar.delegate = self
         calendar.dataSource = self
         
+        
         view.addSubview(calendar)
         setCalendarLayout()
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(swipeEvent(_:)))
+        swipeUp.direction = .up
+        self.view.addGestureRecognizer(swipeUp)
+
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(swipeEvent(_:)))
+        swipeDown.direction = .down
+        self.view.addGestureRecognizer(swipeDown)
     }
     
     func setCalendarLayout(){
@@ -38,6 +51,25 @@ class todoViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSo
             $0.width.equalToSuperview()
             $0.height.equalToSuperview()
             $0.top.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    @objc func swipeEvent(_ swipe: UISwipeGestureRecognizer) {
+
+        if swipe.direction == .up {
+            calendar.setScope(.week, animated: true)
+        } else if swipe.direction == .down {
+            calendar.setScope(.month, animated: true)
+        }
+    }
+}
+
+extension todoViewController: FSCalendarDelegate, FSCalendarDataSource {
+    
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+       
+        UIView.animate(withDuration: 0.5) {
+            self.view.layoutIfNeeded()
         }
     }
 }
